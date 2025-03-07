6ebfbb5260b3887f9ab3e5efafc8becc3fb4f25a
renan <renan>
2025-03-06 22:32:17 -0300

    Projeto de tela de login e registro

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Login from "./Components/Login/Login";
+import Registrar from "./Components/Login/Registrar";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Registrar/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Registrar.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import {FaUser, FaLock, FaFacebook, FaGithub, FaLinkedin} from "react-icons/fa";
+import "./Registrar.css";
+
+    const Registrar = () => {
+        const [nomeCompleto, setNomeCompleto] = useState("");
+        const [idade, setIdade] = useState("");
+        const [facebook, setFacebook] = useState("");
+        const [email, setEmail] = useState("");
+        const [github, setGithub] = useState("");
+        const [linkedln, setLinkedln] = useState("");
+        const [senha, setSenha] = useState("");
+
+         // Função que é chamada quando o formulário é enviado
+         const handleSubmit = (event) => {
+            event.preventDefault();
+            console.log("Dados de cadastro: ", { nomeCompleto, idade, facebook, email, github, linkedln, senha})       
+         };
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Registre-se no sistema</h1>
+                <div className="input-field">
+                    <input
+                     type="text"
+                     placeholder="Nome"
+                     required
+                     value={nomeCompleto}
+                     onChange={(e) => setNomeCompleto(e.target.value)}
+                     />
+                     <FaUser className="icon"/>
+                </div>
+                <div className="input-field">
+                <input 
+                type="number"
+                placeholder="Idade"
+                required
+                value={idade}
+                onChange={(e) => setIdade(e.target.value)}
+                />
+                </div>
+                <div className="input-field">
+                    <input 
+                    type="text"
+                    placeholder="Facebook"
+                    required
+                    value={facebook}
+                    onChange={(e) => setFacebook(e.target.value)}
+                     />
+                     <FaFacebook className="icon"/>
+                </div>
+                <div className="input-field">
+                    <input 
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-field">
+                    <input 
+                    type="text"
+                    placeholder="Github"
+                    required
+                    value={github}
+                    onChange={(e) => setGithub(e.target.value)}
+                    />
+                    <FaGithub className="icon"/>
+                </div>
+                <div className="input-field">
+                    <input 
+                        type="text"
+                        placeholder="Linkedln"
+                        required
+                        value={linkedln}
+                        onChange={(e) => setLinkedln(e.target.value)}
+                    />
+                    <FaLinkedin className="icon"/>
+                </div>
+                <div className="input-field">
+                    <input 
+                    type="text"
+                    placeholder="Senha"
+                    required
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                    />
+                    <FaLock className="icon"/>
+                </div>
+                <button type="submit"> Criar conta</button>
+
+                <div className="login-link">
+                    <p>
+                        Já tem uma conta? <a href="Login.jsx">Entrar</a>
+                    </p>
+                </div>
+            
+                
+            </form>
+        </div>
+    );
+
+
+    } 
+    export default Registrar;
+
